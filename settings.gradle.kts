5bc51547d0356d8238f192095ba019b322679802
Behnam Banaei <behnam.banaei@tapsi.cab>
2025-12-03 20:49:08 +0330

    add processor and wiring to demo project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "KspPlayground-minimal"
+include("grpc-processor")
+include("grpc-processor")
+include("demo")
