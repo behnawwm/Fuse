c5b176c61721ad1414fb027640523af82fce8b1b
Behnam Banaei <behnam.banaei@tapsi.cab>
2025-12-10 14:19:19 +0330

    added grpc processor and featuretoggle processor

---
+++ demo/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm")
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+    id("com.squareup.wire")
+}
+
+group = "com.behnawwm"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.squareup.wire:wire-grpc-client:5.4.0")
+    implementation("com.squareup.wire:wire-schema:5.4.0")
+
+    ksp(project(":tapsi-grpc-processor"))
+    ksp(project(":tapsi-featuretoggle-processor"))
+    implementation(project(":tapsi-grpc-annotations"))
+    implementation(project(":tapsi-featuretoggle-annotations"))
+
+}
+wire {
+    kotlin {}
+}
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ tapsi-grpc-processor/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm")
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+}
+
+group = "com.behnawwm"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(project(":tapsi-grpc-annotations"))
+    implementation("com.google.devtools.ksp:symbol-processing-api:2.2.0-2.0.2")
+    implementation("com.squareup:kotlinpoet:1.14.2")
+    implementation("com.squareup:kotlinpoet-ksp:1.14.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
